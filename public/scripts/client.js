394f281a3df5633c0e4a49c7caaa7a6e1fa98be2
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-27 12:15:37 -0400

    refactor code for readability

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,60 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+      },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+ }
+
+
+ // Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+ const createTweetElement = function(tweetObject) {
+ //const $tweet = createTweetElement(tweetData);
+// Test / driver code (temporary)
+//console.log($tweet); // to see what it looks like
+//$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+//let tweet = 
+
+}
+
+const renderTweets = function (tweetArray) {
+    tweetAray.forEach((element) => {
+      createTweetElement(element);
+
+     });
+
+   });
+
+renderTweets(data);
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,14 +3,13 @@ $(document).ready(function() {
 
   // when tweet-text is clicked, run the function
   $("#tweet-text").keyup(function(event) {
- 
-    const countField = $('output[name="counter"]');
-          
+    const $countField = $(this.form.elements.counter);
+  
     //set the count to MAX - tweet length
-    countField.text(MAX_TWEET - this.value.length);
+    $countField.text(MAX_TWEET - this.value.length);
         
-    if (Number(countField.text()) < 0) {
-      countField.addClass('red');
+    if (Number($countField.text()) < 0) {
+      $countField.addClass('red');
     }
   });
 });
